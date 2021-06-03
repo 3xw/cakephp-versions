064fd1e89cc855a190e28bed1569cfdfc286e317
awallef <antoine.wallef@gmail.com>
2021-06-03 09:43:55 +0200

    more

--- src/Model/Behavior/VersionsBehavior.php
+++ src/Model/Behavior/VersionsBehavior.php
@@ -12,6 +12,7 @@ use Cake\Core\Configure;
 use Cake\Utility\Text;
 use Cake\Datasource\ModelAwareTrait;
 use Cake\Routing\Router;
+use Cake\Utility\Hash;
 
 /**
 * Versions behavior
@@ -32,7 +33,7 @@ class VersionsBehavior extends Behavior
       'max_amount' => 10, // -1 => no limit
       'max_periode' => '12 months', // -1 => no limit
     ],
-    'fields' => ['content'],
+    'fields' => [],
     'version_field' => '_versions',
   ];
 
@@ -44,23 +45,90 @@ class VersionsBehavior extends Behavior
 
   public function afterSave(Event $event, EntityInterface $entity, ArrayObject $options)
   {
-    $entity->set(
-      $this->getConfig('version_field'),
-      $this->createVersions($entity, $this->getConfig('fields'))
-    );
+    // try save version of each fields
+    if(!$versions = $this->createVersions($entity, $this->getConfig('fields')))
+    {
+      $event->stopPropagation();
+      return false;
+    }
+
+    // attach current version
+    $this->attachVersions($entity, $versions);
+
     return $entity;
   }
 
   public function afterDelete(Event $event, EntityInterface $entity, ArrayObject $options)
   {
-    $versions = $entity->get($this->getConfig('version_field'));
-
-    if($versions && !empty($versions)) $this->Versions->deleteAll([
+    // set conditions
+    $conditions = [
       'model' => $this->table()->getAlias(),
       'foreign_key' => $entity->{ $this->table()->getPrimarykey() },
-    ]);
+    ];
+
+    // count rows
+    if(!$count = $this->Versions->find()->where($conditions)->count()) return $entity;
+
+    // delete rows and check
+    if($this->Versions->deleteAll($conditions) < $count)
+    {
+      $event->stopPropagation();
+      return false;
+    }
 
     return $entity;
+  }
+
+  protected function attachVersions(EntityInterface $entity, $versions)
+  {
+    $structuredVersions = $this->toStructVersionsArray($versions);
+    foreach($structuredVersions as $datetime => &$localesArray)
+    {
+      foreach($localesArray as $locale => &$verionFiledsArray)
+      {
+        // create entioty from current entity state
+        $data = $entity->toArray();
+        if(!empty($data['_translations'])) unset($data['_translations']);
+        $ve = $this->table()->newEntity($data);
+
+        // merge with current state locale
+        $trans = $entity->get('_translations');
+        if( is_array($trans) && Hash::check($trans, $locale)) $ve = $this->table()->patchEntity($ve, $trans[$locale]->toArray());
+
+        // modify entity with version state
+        foreach($verionFiledsArray as $vf) $ve->set($vf->field, $vf->content);
+
+        // set version owner ship
+        $ve->set('owned_by', $vf->user?? $vf->user_id);
+
+        // set entity
+        $verionFiledsArray = $ve;
+      }
+    }
+
+    $entity->set($this->getConfig('version_field'), $structuredVersions);
+  }
+
+  protected function toStructVersionsArray(array $versions)
+  {
+    $sv = [];
+    foreach($versions as $version)
+    {
+      $v = is_array($version)? $this->Versions->newEntity($version): $version;
+      $created = $v->created->format('Y-m-d H:i:s');
+      $path = "$created.$v->locale";
+
+      // create dim created.locale
+      if(Hash::check($sv, $path)) $sv = Hash::merge($sv, Hash::expand([$path => []]));
+
+      // add field for created.locale
+      $sv[$created][$v->locale][] = $v;
+    }
+
+    // sort array by created DESC
+    krsort($sv);
+
+    return $sv;
   }
 
   protected function tryGetUserId()
